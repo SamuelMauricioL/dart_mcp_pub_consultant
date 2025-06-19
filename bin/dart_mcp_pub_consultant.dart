2608f6a76a908936bdabfcc56a3bbe620c5e5df1
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2025-06-19 13:39:01 -0500

    feat: init project

---
+++ bin/dart_mcp_pub_consultant.dart
@@ -0,0 +1,5 @@
+import 'package:dart_mcp_pub_consultant/dart_mcp_pub_consultant.dart' as dart_mcp_pub_consultant;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_mcp_pub_consultant.calculate()}!');
+}
